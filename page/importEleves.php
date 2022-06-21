d017cadb7bf8c8e2f2ade2fe6f617d4410118073
EscolanoA <escolanoallan4@gmail.com>
2022-06-21 10:52:43 +0200

    Importation CSV

--- page/importEleves.php
+++ page/importEleves.php
@@ -8,6 +8,9 @@
 
 <?php
 
+        $NomsPrenoms = [];
+        $Dates = [];
+
     if(isset( $_FILES['file'])) {
         $csvFile = $_FILES['file'];
     }
@@ -22,10 +25,51 @@
             for ($v=0; $v<count($csvAsArray[0]); $v++) { // Pour chaque colonne de la ligne
                 //print_r($csvAsArray[$i][$v]);
                 echo("<td>".$csvAsArray[$i][$v]."</td>");
+                if(preg_match('(Nom|nom|Prénom|Prenom|prenom|prénom)', $csvAsArray[$i][$v]) === 1) {
+                    $ColonnePrenomsNoms = $v; // Recupere la colonne des prenoms
+                }
+
+                if(preg_match('(Date du oui)', $csvAsArray[$i][$v]) === 1) {
+                    $ColonneDates = $v;
+                }
+
+                if(isset($ColonnePrenomsNoms)) { //Si la colonne a été trouver
+                    if($i > 1) { //Si la ligne n'est pas la premiere ligne (car la peremiere ligne est la legende)
+                        if($v == $ColonnePrenomsNoms) { //Si l'incrément arrive sur la colonne des prenoms
+                            array_push($NomsPrenoms, $csvAsArray[$i][$v]); //Rajouter le nom et prenom dans le tableau
+                        }
+                    }
+                }
+
+                if(isset($ColonneDates)) { //Si la colonne a été trouver
+                    if($i > 1) { //Si la ligne n'est pas la premiere ligne (car la peremiere ligne est la legende)
+                        if($v == $ColonneDates) { //Si l'incrément arrive sur la colonne des prenoms
+                            array_push($Dates, $csvAsArray[$i][$v]); //Rajouter le nom et prenom dans le tableau
+                        }
+                    }
+                }
+
             }
 
             echo("</tr>");
         }
 
     echo("</table>");
+
+    for($i = 0; $i < count($NomsPrenoms); $i++) { //Pour la longeur du tableau
+        $NomPrenom = explode(" ", $NomsPrenoms[$i]); //Séparé la string en deux partie au niveau de l'éspace
+        $Nom = $NomPrenom[0]; //Recupere la premiere partie de la string aka le nom
+        $Prenom = $NomPrenom[1]; //Recupere la deuxieme partie de la string aka le prenom
+        //print_r($Nom);
+        //print_r($Prenom);
+    }
+    //print_r($NomsPrenoms)
+
+    for($i = 0; $i < count($Dates); $i++) { //Pour la longeur du tableau
+        $Date = explode(" ", $Dates[$i]); //Séparé la string en plusieurs partie au niveau de l'éspace
+        $Annee = $Date[2]; //Recupere la troisieme partie de la string aka l'année
+        //print_r($Annee);
+    }
+
+    //print_r($Dates);
 ?>
