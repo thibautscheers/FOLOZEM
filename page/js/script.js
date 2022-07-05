f8c095022d2577baa30e3438e5d58479bb35bea2
Thibaut scheers <thibaut.scheers.sn@gmail.com>
2022-07-05 11:53:54 +0200

    Ajout entête en comm pour CSS et JS

--- page/js/script.js
+++ page/js/script.js
@@ -1,3 +1,5 @@
+// Page du JS créée et géré par Thibaut SCheers
+// fonction pour la déconnecxion du site
 function deco() {
     document.getElementById("deco")
     if (confirm("Voulez vous vous déconnecté?")) {
